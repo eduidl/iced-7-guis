ea98cb447b9ff728733ff5d6b6a34f3008c225dc
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2023-03-11 12:41:51 +0900

    Add counter

---
+++ src/bin/counter.rs
@@ -0,0 +1,55 @@
+use iced::{
+    alignment::Horizontal,
+    widget::{button, row, text},
+    window::{self, Position},
+    Element, Sandbox, Settings,
+};
+
+#[derive(Debug, Clone, Copy)]
+struct Increment;
+
+#[derive(Default)]
+struct Counter {
+    value: i32,
+}
+
+impl Sandbox for Counter {
+    type Message = Increment;
+
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter")
+    }
+
+    fn view(&self) -> Element<Increment> {
+        row![
+            text(self.value)
+                .size(30)
+                .width(80)
+                .horizontal_alignment(Horizontal::Right),
+            button("Count").on_press(Increment),
+        ]
+        .spacing(10)
+        .padding(10)
+        .into()
+    }
+
+    fn update(&mut self, _: Increment) {
+        self.value += 1;
+    }
+}
+
+fn main() -> iced::Result {
+    Counter::run(Settings {
+        window: window::Settings {
+            size: (180, 50),
+            position: Position::Centered,
+            resizable: false,
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+}
